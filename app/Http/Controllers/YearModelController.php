f6a000d8944a4f76be49a8a793566c8463181d18
Taylor Hicks <taylor@whatsmyoffer.com>
2022-01-27 22:16:21 -0600

    Generated Models,Factories,Seeders,Controllers for all DB Tables

---
+++ app/Http/Controllers/YearModelController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class YearModelController extends Controller
+{
+    //
+}
---
+++ database/seeders/YearModelExteriorColorSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class YearModelExteriorColorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
