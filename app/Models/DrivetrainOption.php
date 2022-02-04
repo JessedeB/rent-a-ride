13ac478d581a2d5ffda2bedbe9cdb1fc79d87243
Taylor Hicks <taylor@whatsmyoffer.com>
2022-02-03 21:04:50 -0600

    Basic Relationships for DrivetrainOption Model

--- app/Models/DrivetrainOption.php
+++ app/Models/DrivetrainOption.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class DrivetrainOption extends Model
 {
     use HasFactory;
+
+    public function yearModel()
+    {
+        return $this->hasOne(YearModel::class);
+    }
+
+    public function vehicles()
+    {
+        return $this->belongsToMany(Vehicle::class);
+    }
+
 }
