a016963500ffeb3effc46b8617e852645cbcd6fe
Taylor Hicks <taylor@whatsmyoffer.com>
2022-02-03 21:17:09 -0600

    Added return types

--- app/Models/DrivetrainOption.php
+++ app/Models/DrivetrainOption.php
@@ -4,17 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class DrivetrainOption extends Model
 {
     use HasFactory;
 
-    public function yearModel()
+    public function yearModel() : HasOne
     {
         return $this->hasOne(YearModel::class);
     }
 
-    public function vehicles()
+    public function vehicles() : BelongsToMany
     {
         return $this->belongsToMany(Vehicle::class);
     }
--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -4,22 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Manufacturer extends Model
 {
     use HasFactory;
 
-    public function yearModels()
+    public function yearModels() : BelongsToMany
     {
         return $this->belongsToMany(YearModel::class);
     }
 
-    public function exteriorColors()
+    public function exteriorColors() : BelongsToMany
     {
         return $this->belongsToMany(ExteriorColor::class);
     }
 
-    public function interiorColors()
+    public function interiorColors() : BelongsToMany
     {
         return $this->belongsToMany(InteriorColor::class);
     }
