ace59b20dfcae919e2a53a548c3f6d60bbbe3b81
Taylor Hicks <taylor@whatsmyoffer.com>
2022-02-03 21:20:34 -0600

    Basic relationships for InteriorColor Model

--- app/Models/InteriorColor.php
+++ app/Models/InteriorColor.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class InteriorColor extends Model
 {
     use HasFactory;
+
+    public function manufacturer() : HasOne
+    {
+        return $this->hasOne(Manufacturer::class);
+    }
+
+    public function yearModels() : HasManyThrough
+    {
+        return $this->hasManyThrough(YearModel::class,YearModelInteriorColor::class);
+    }
+
+    public function yearModeInteriorColors() : BelongsToMany
+    {
+        return $this->belongsToMany(YearModelInteriorColor::class);
+    }
+
+    public function vehicles() : BelongsToMany
+    {
+        return $this->belongsToMany(Vehicle::class);
+    }
 }
