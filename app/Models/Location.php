16917fa660fb8f35632b6f9a1f1c8f571ec8c09f
Taylor Hicks <taylor@whatsmyoffer.com>
2022-02-03 22:19:56 -0600

    Basic relationships for Locations Model

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Location extends Model
 {
     use HasFactory;
+
+    public function rentalPickups() : BelongsToMany
+    {
+        return $this->belongsToMany(Rental::class,'rentals','pickup_location','id');
+    }
+
+    public function rentalReturns() : BelongsToMany
+    {
+        return $this->belongsToMany(Rental::class,'rentals','return_location','id');
+    }
+
+    public function vehicles(): BelongsToMany
+    {
+        return $this->belongsToMany(Vehicle::class);
+    }
 }
