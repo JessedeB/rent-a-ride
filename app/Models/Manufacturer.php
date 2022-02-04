fba1993ec78a868721a5323370307474e3a48e22
Taylor Hicks <taylor@whatsmyoffer.com>
2022-02-03 20:57:02 -0600

    Relationships for Manufacturer Model

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Manufacturer extends Model
 {
     use HasFactory;
+
+    public function yearModels()
+    {
+        return $this->belongsToMany(YearModel::class);
+    }
+
+    public function exteriorColors()
+    {
+        return $this->belongsToMany(ExteriorColor::class);
+    }
+
+    public function interiorColors()
+    {
+        return $this->belongsToMany(InteriorColor::class);
+    }
+
+
 }
