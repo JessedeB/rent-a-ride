98a69470a909f659e4467393961043640ce318da
Taylor Hicks <taylor@whatsmyoffer.com>
2022-02-03 22:23:07 -0600

    Basic relationships for Payment Model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Payment extends Model
 {
     use HasFactory;
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function rental(): HasOne
+    {
+        return $this->hasOne(Rental::class);
+    }
 }
