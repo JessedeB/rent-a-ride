3ce83fa9288982a0bb9be54eadf03f754cff3e0d
Taylor Hicks <taylor@whatsmyoffer.com>
2022-02-11 17:05:58 -0600

    colors.create view

--- resources/views/dashboard/vehicles/colors/create.blade.php
+++ resources/views/dashboard/vehicles/colors/create.blade.php
@@ -5,12 +5,29 @@
 @include('layouts.partials.message')
 
 <x-card>
-    <form action="{{ route('permissions.store') }}" method="POST">
+    <form action="{{ route("$type-colors.store") }}" method="POST">
         @csrf
 
         <div class="mb-3">
+            <label for="make">Make</label>
+            <select id="make" name="manufacturer_id" class="form-select">
+                @forelse($manufacturers as $manufacturer)
+                    @if($loop->first)
+                        <option id="make-default" value="null">Select a make</option>
+                    @endif
+                    <option value="{{$manufacturer->id}}">{{$manufacturer->make}}</option>
+                @empty
+                    <option value="null">No Makes available. Add a make first.</option>
+                @endforelse
+            </select>
+            @error('manufacturer_id')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
+        </div>
+
+        <div class="mb-3">
             <label for="name">Name</label>
-            <input type="text" name="name" id="name" class="form-control" placeholder="Permission name">
+            <input type="text" name="name" id="name" class="form-control" placeholder="Color Name">
 
             @error('name')
             <p class="text-danger">{{$message}}</p>
@@ -18,14 +35,13 @@
         </div>
 
         <div class="mb-3">
-            <label for="guard_name">Guard name</label>
-            <input type="text" name="guard_name" id="guard_name" class="form-control" placeholder="Guard name">
+            <label for="hex_code">Hex Code</label>
+            <input type="text" hex_code="hex_code" id="hex_code" class="form-control" placeholder="Hex Code">
 
-            @error('guard')
+            @error('hex_code')
             <p class="text-danger">{{$message}}</p>
             @enderror
         </div>
-
         <button type="submit" class="btn btn-primary">Create</button>
     </form>
 </x-card>
